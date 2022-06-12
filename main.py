b0e02ee26bf93930348c3a1282f29b80e59d2e2f
bimardwi <bimarax@gmail.com>
2022-06-12 20:47:06 +0800

    added main file

---
+++ main.py
@@ -0,0 +1,20 @@
+from snake import Snake
+from arena import *
+from clear import clear
+from collision import collision
+import time
+
+
+snake = Snake()
+
+while True:
+    snake.direction()
+    snake.move()
+    if collision(snake.head_before) is True:
+        break
+    board = arena(snake.head_before, snake.body_before)
+    print(printing(board))
+    time.sleep(0.1)
+    clear()
+
+print("mati nabrak dinding aot")
