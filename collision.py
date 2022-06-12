f0d2223f54636404cd34d63f000adb208eae995a
bimardwi <bimarax@gmail.com>
2022-06-12 22:36:03 +0800

    added new algorithm to check if the snake eat itself

--- collision.py
+++ collision.py
@@ -1,8 +1,10 @@
-def collision(snake_head):
-    vertical_pos, horizontal_pos = snake_head
+def collision(head, body):
+    vertical_pos, horizontal_pos = head
     if vertical_pos == 0 or vertical_pos == 27:
         return True
     elif horizontal_pos == 0 or horizontal_pos == 27:
+        return True
+    elif head in body:
         return True
     else:
         return False
