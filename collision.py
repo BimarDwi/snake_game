294740be64b22be91e8b8efec7c83f917e57328d
bimardwi <bimarax@gmail.com>
2022-06-12 22:47:39 +0800

    fixed if statement in horizontal_pos and vertical_pos from 27 to 26

--- collision.py
+++ collision.py
@@ -1,8 +1,8 @@
 def collision(head, body):
     vertical_pos, horizontal_pos = head
-    if vertical_pos == 0 or vertical_pos == 27:
+    if vertical_pos == 0 or vertical_pos == 26:
         return True
-    elif horizontal_pos == 0 or horizontal_pos == 27:
+    elif horizontal_pos == 0 or horizontal_pos == 26:
         return True
     elif head in body:
         return True
