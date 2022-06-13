9bca327230a67846f8d57553bf4b2f71ca8b40cd
bimardwi <bimarax@gmail.com>
2022-06-13 23:01:47 +0800

    turned off collision with wall

--- collision.py
+++ collision.py
@@ -1,10 +1,10 @@
 def collision(head, body):
-    vertical_pos, horizontal_pos = head
-    if vertical_pos == 0 or vertical_pos == 26:
-        return True
-    elif horizontal_pos == 0 or horizontal_pos == 26:
-        return True
-    elif head in body:
+    # vertical_pos, horizontal_pos = head
+    # if vertical_pos == 0 or vertical_pos == 26:
+    #     return True
+    # elif horizontal_pos == 0 or horizontal_pos == 26:
+    #     return True
+    if head in body:
         return True
     else:
         return False
